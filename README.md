2932c96599775309bd59407058d27f5423256625
brunesto <bruno.carle@gmail.com>
2025-01-08 18:20:47 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # flask-session-sample
 small sample for flask session usage
+
+
+```
+  python -m venv .venv
+  . .venv/bin/activate
+  pip install -r requirements.txt
+```
+
+run with flask dev env
+```
+python -m flask  --app app2 run
+# or
+python app2
+```
+
+
+run with gunicorn
+```
+gunicorn -w 4 -b 0.0.0.0:5000 app2:app2
+
+```
+
+run with waitress
+```
+waitress-serve --host 0.0.0.0 --port 5000  app2:app2
+```
---
+++ app2.py
@@ -0,0 +1,54 @@
+from flask import Flask, session, render_template, request
+
+
+app2 = Flask(__name__)
+app2.secret_key = "123"
+
+
+@app2.context_processor
+def inject_enumerate():
+    return dict(enumerate=enumerate)
+
+
+@app2.route("/", methods=["GET"])
+def index():
+    messages = session.get("messages", [])
+    return render_template("index2.html", messages=messages)
+
+
+@app2.route("/clear", methods=["POST"])
+def clear_messages():
+    session["messages"] = []
+    return index()
+
+
+@app2.route("/add", methods=["POST"])
+def add_message():
+    messages = session.get("messages", [])
+    messages.append(request.form["new_message"])
+    session["messages"] = messages
+    return index()
+
+
+# code below is not necessary for the example
+
+
+# initializing code before every request
+@app2.before_request
+def before_request():
+    # ... useful code here
+    print("initialize")
+
+
+# initializing code once for the whole app - does not do anything
+@app2.before_request
+def initializeApp():
+    # ... useful code here
+    print("initializeApp")
+    app2.before_request_funcs[None].remove(initializeApp)
+
+
+# convenient method to run in development environment with:
+# python app2.py
+if __name__ == "__main__":
+    app2.run()
